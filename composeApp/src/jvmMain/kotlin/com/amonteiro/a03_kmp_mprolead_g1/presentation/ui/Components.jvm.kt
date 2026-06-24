a7eccc1e9b031bc12979a2fb4d2a722cec5e0987
EnzoTlz <enzo31del@gmail.com>
2026-06-24 13:34:08 +0200

    first commit

---
+++ composeApp/src/jvmMain/kotlin/com/amonteiro/a03_kmp_mprolead_g1/presentation/ui/Components.jvm.kt
@@ -0,0 +1,11 @@
+package com.amonteiro.a03_kmp_mprolead_g1.presentation.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+actual fun PictureGallery(
+    modifier: Modifier,
+    urlList: List<String>
+) {
+}
---
+++ composeApp/src/nativeMain/kotlin/com/amonteiro/a03_kmp_mprolead_g1/presentation/ui/Components.native.kt
@@ -0,0 +1,8 @@
+package com.amonteiro.a03_kmp_mprolead_g1.presentation.ui
+
+@androidx.compose.runtime.Composable
+actual fun PictureGallery(
+    modifier: androidx.compose.ui.Modifier,
+    urlList: List<String>
+) {
+}
